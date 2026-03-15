e06163d672d3d6b6c6c76e60353f941fb4e41486
guruabhay25 <guptaabhay2727@gmail.com>
2026-03-16 00:19:28 +0530

    SpotifyClone Done by cwh

--- Spotify/script.js
+++ Spotify/script.js
@@ -9,6 +9,7 @@ let songItemPause = document.querySelectorAll('.songItemPause');
 let songItemPlay = document.querySelectorAll('.songItemPlay');
 let next = document.getElementById('next');
 let previous = document.getElementById('previous');
+let masterSongName = document.getElementById('masterSongName');
 
 let songs = [
     {songName: "Pehle bhi", filePath: "Songs/Pehle-bhi.mp3", coverPath: "Images/1.jpg"},
@@ -65,6 +66,7 @@ songItemPlay.forEach((element, i)=>{
         element.style.display = 'none';
         songItemPause[i].style.display = 'inline';
         audioElement.src = songs[songIndex].filePath;
+        masterSongName.innerText = songs[songIndex].songName;
         audioElement.currentTime = 0;
         audioElement.play();
         masterPlay.style.display = 'none';
@@ -73,7 +75,49 @@ songItemPlay.forEach((element, i)=>{
     })
 });
 
+songItemPause.forEach((element, i)=>{
+    element.addEventListener('click',()=>{
+        songIndex = parseInt(element.id);
+        element.style.display = 'none';
+        songItemPlay[i].style.display = 'inline';
+        audioElement.currentTime = 0;
+        audioElement.pause();
+        masterPlay.style.display = 'inline';
+        masterPause.style.display = 'none';
+        gif.style.opacity = 0;
+    })
+});
+
 next.addEventListener('click', () =>{
-    
+    if(songIndex>=2){
+        songIndex = 0;
+    }
+    else{
+        songIndex += 1;
+    }
+    audioElement.src = songs[songIndex].filePath;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.style.display = 'none';
+    masterPause.style.display = 'inline';
+    gif.style.opacity = 1;
 })
+
+previous.addEventListener('click', () =>{
+    if(songIndex<=0){
+        songIndex = 0;
+    }
+    else{
+        songIndex -= 1;
+    }
+    audioElement.src = songs[songIndex].filePath;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.style.display = 'none';
+    masterPause.style.display = 'inline';
+    gif.style.opacity = 1;
+})
+
 
