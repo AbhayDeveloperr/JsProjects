e0fa9f104d0d9ec9163b99589bb4ee89f7b1a76f
guruabhay25 <guptaabhay2727@gmail.com>
2026-03-26 00:19:57 +0530

    styling of snake game

---
+++ Snake-Game/script.js
@@ -0,0 +1,23 @@
+//Game constants and variables
+let direction = {x:0 , y:0};
+const foodSound = new Audio('images/food.mp3');
+const gameOverSound = new Audio('images/gameover.mp3');
+const moveSound = new Audio('images/move.mp3');
+const musicSound = new Audio('images/music.mp3');
+
+//Game Functions
+function main(cTime){
+    window.requestAnimationFrame(main);                    //ab ye loop me chlega
+    // console.log(cTime);
+}
+
+
+
+
+
+
+
+
+
+//Main logic starts here
+window.requestAnimationFrame(main);
